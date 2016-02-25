e7996d20ffbcd67f399ebedf622929119d7b7e19
juanvisanc <juanfransanc@gmail.com>
2016-02-25 18:16:14 +0100

    Arreglado fallito

--- usuario/calendario.php
+++ usuario/calendario.php
@@ -131,7 +131,8 @@
             echo "<tr><form method='POST' action='../admin/guarda_resultado.php'>
             <input type='number' value='$obj->idPartido' name='id' style='display:none' required/>
             <input type='number' value='$id' name='jornada' style='display:none' required/>
-            <td><a href='../usuario/estadistica.php?idEq=$obj->lo&idP=$obj->idPartido'>$obj->local</a></td><td><input type='number' class='form-control' value='$obj->golesL' name='golesL' required></td>
+            <td><a href='../usuario/estadistica.php?idEq=$obj->lo&idP=$obj->idPartido'>$obj->local</a></td>
+            <td><input type='number' class='form-control' value='$obj->golesL' name='golesL' min='0' required></td>
             <td><input type='number' class='form-control' value='$obj->golesV' name='golesV' min='0' required></td>
             <td><a href='../usuario/estadistica.php?idEq=$obj->vi&idP=$obj->idPartido'>$obj->visitante</a></td><td>$obj->fecha</td>
             <td><a href='../admin/elimina_partido.php?id=$obj->idPartido&jornada=$id'>
